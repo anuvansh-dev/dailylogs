fea2b4698652411085421c4abb86767f7d9ffa15
anuvansh-dev <kumaranuvansh@seasiaconnect.com>
2025-03-26 18:05:45 +0530

    Image upload functionality added in project, all the modified files are attached

---
+++ project-image-upload-feature-modified-files/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+class Project(models.Model):
+    STATUS_CHOICES = [('Voting Open', 'Voting Open'), ('Active', 'Active'), 
+                      ('Discarded', 'Discarded'), ('Completed', 'Completed')]
+    
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Voting Open')
+    created_at = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('user', 'project') # Ensures a user votes only once per project
+        
+    
+class FacilityRequest(models.Model):
+    CATEGORY_CHOICES = [('Roads', 'Roads'), ('Water Supply', 'Water Supply'), 
+                        ('Electricity', 'Electricity'), ('Parks', 'Parks'), ('Cleaning', 'Cleaning'), 
+                        ('Other', 'Other')]
+    
+    STATUS_CHOICES = [('Pending', 'Pending'), ('Approved', 'Approved'), 
+                      ('Rejected', 'Rejected'), ('Completed', 'Completed')]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    image = models.ImageField(upload_to='facility_request_images/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.category} ({self.status})"
+    
+
+class Feedback(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, null=True, blank=True, on_delete=models.CASCADE)
+    facility_request = models.ForeignKey(FacilityRequest, null=True, blank=True, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(1, 'Poor'), (2, 'Fair'), (3, 'Good'), (4, 'Very Good'), (5, 'Excellent')])
+    comments = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        if self.project:
+            return f"Feedback for Project: {self.project.name} by ({self.user.username} - {self.rating})"
+        elif self.facility_request:
+            return f"Feedback for Facility Request: {self.facility_request} by ({self.user.username} - {self.rating})"
+        else:
+            return f"Feedback by {self.user.username}"
---
+++ project-image-upload-feature-modified-files/serializers.py
@@ -0,0 +1,66 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Project, Vote, Feedback, FacilityRequest
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+class ProjectSerializer(serializers.ModelSerializer):
+    vote_count = serializers.IntegerField(read_only=True)
+    class Meta:
+        model = Project
+        fields = '__all__'
+        
+        
+    def to_representation(self, instance):
+        """ Custom representation to include vote count """
+        data = super().to_representation(instance)
+        data["vote_count"] = Vote.objects.filter(project=instance).count()
+        return data
+
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = '__all_'
+        
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feedback
+        fields = '__all__'
+        
+        
+class FacilityRequestSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="user.username", read_only=True)
+    feedback = serializers.SerializerMethodField()
+    image_url = serializers.SerializerMethodField()
+    image = serializers.ImageField(required=False, allow_null=True) 
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    # returning feedback if exists with each api response
+    def get_feedback(self, obj):
+        feedback = Feedback.objects.filter(facility_request=obj).first()
+        if feedback:
+            return {
+                "rating": feedback.rating,
+                "comments": feedback.comments
+            }
+        return None  # No feedback submitted yet
+    
+    # returning url of the uploaded image 
+    def get_image_url(self, obj):
+        if obj.image:
+            request = self.context.get("request")
+            if request is not None:
+                return request.build_absolute_uri(obj.image.url)  # Return absolute URL
+            return obj.image.url
+        return None  # Return None if no image uploaded
+    
+    class Meta:
+        model = FacilityRequest
+        fields = ['id', 'user', 'username', 'category', 'description', 'status', 'image', 'image_url', 'feedback', 'created_at']
